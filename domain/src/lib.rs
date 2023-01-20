6a4376059c6419b6b0712f87b564df9f04be59b7
Frank Moreno <frankmoreno1993@gmail.com>
2023-01-19 22:04:58 -0500

    domain fixed

--- domain/src/lib.rs
+++ domain/src/lib.rs
@@ -2,10 +2,16 @@ pub use async_trait::async_trait;
 
 #[async_trait]
 pub trait UserRepo {
-    type User: Clone;
-    type UserId;
+    type User: User;
 
     async fn create_user(&mut self, name: String) -> Self::User;
-    async fn get_users(&self) -> Vec<Self::User>;
-    async fn get_user_by_id(&self, id: Self::UserId) -> Option<Self::User>;
+    async fn get_users(&self) -> &Vec<Self::User>;
+    async fn get_user_by_id(&self, id: <Self::User as User>::Id) -> Option<Self::User>;
+}
+
+pub trait User: Clone {
+    type Id: From<u32> + Into<u32>;
+
+    fn id(&self) -> Self::Id;
+    fn name(&self) -> String;
 }
