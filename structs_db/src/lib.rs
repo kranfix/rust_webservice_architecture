3a377ffa91dc698eb2795d8b5caf5651409c900d
Frank Moreno <frankmoreno1993@gmail.com>
2023-01-19 22:05:12 -0500

    structs_db implements domain

--- structs_db/src/lib.rs
+++ structs_db/src/lib.rs
@@ -1,32 +1,49 @@
 use domain::{async_trait, UserRepo};
 
-#[derive(Clone)]
-pub struct User {
-    pub id: u32,
-    pub name: String,
+pub struct UserDB(Vec<User>);
+
+impl UserDB {
+    pub fn new() -> UserDB {
+        UserDB(Vec::new())
+    }
 }
 
-struct UserDB(Vec<User>);
+#[derive(Clone)]
+pub struct User {
+    pub id: u64,
+    pub nick: String,
+}
+
+impl domain::User for User {
+    type Id = u32;
+
+    fn id(&self) -> Self::Id {
+        self.id as u32
+    }
+
+    fn name(&self) -> String {
+        self.nick.clone()
+    }
+}
 
 #[async_trait]
 impl UserRepo for UserDB {
     type User = User;
-    type UserId = u32;
 
     async fn create_user(&mut self, name: String) -> Self::User {
         let user = User {
-            id: self.0.len() as u32 + 1,
-            name,
+            id: self.0.len() as u64 + 1,
+            nick: name,
         };
         self.0.push(user.clone());
         user
     }
 
-    async fn get_users(&self) -> Vec<Self::User> {
-        self.0.clone()
+    async fn get_users(&self) -> &Vec<Self::User> {
+        &self.0
     }
 
-    async fn get_user_by_id(&self, id: Self::UserId) -> Option<Self::User> {
+    async fn get_user_by_id(&self, id: <Self::User as domain::User>::Id) -> Option<Self::User> {
         if id == 0 {
             return None;
         }
