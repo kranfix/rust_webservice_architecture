bf2cf2f15e9b1e9029869d4f4b7dbb665451add6
Frank Moreno <frankmoreno1993@gmail.com>
2023-01-31 14:17:01 -0500

    SurrealReqwest in its own file

--- surrealdb_reqwest_adapter/src/lib.rs
+++ surrealdb_reqwest_adapter/src/lib.rs
@@ -1,65 +1,11 @@
+pub mod client;
 pub mod query_result;
+
+pub use client::SurrealReqwest;
+pub use client::*;
 use domain::{async_trait, CreateUserError};
 use query_result::QueryResult;
-use serde::{de::DeserializeOwned, Deserialize};
-
-pub struct SurrealReqwest {
-  ns: String,
-  db: String,
-  addr: String,
-  auth: Auth,
-}
-
-impl SurrealReqwest {
-  pub fn new(
-    ns: impl Into<String>,
-    db: impl Into<String>,
-    addr: impl Into<String>,
-    auth: Auth,
-  ) -> Self {
-    Self {
-      ns: ns.into(),
-      db: db.into(),
-      addr: addr.into(),
-      auth,
-    }
-  }
-
-  pub async fn sql<T: DeserializeOwned>(
-    &self,
-    sql: impl Into<String>,
-  ) -> Result<Vec<QueryResult<T>>, ()> {
-    let client = reqwest::Client::new()
-      .post(format!("{}/sql", self.addr))
-      .basic_auth(self.auth.user.clone(), Some(self.auth.pass.clone()))
-      .header("NS", self.ns.clone())
-      .header("DB", self.db.clone())
-      .header("Accept", "Application/json")
-      .body(sql.into());
-    let resp = client
-      .send()
-      .await
-      .unwrap()
-      .json::<Vec<QueryResult<T>>>()
-      .await
-      .unwrap();
-    Ok(resp)
-  }
-}
-
-pub struct Auth {
-  user: String,
-  pass: String,
-}
-
-impl Auth {
-  pub fn new(user: impl Into<String>, pass: impl Into<String>) -> Auth {
-    Auth {
-      user: user.into()[7..].into(),
-      pass: pass.into(),
-    }
-  }
-}
+use serde::Deserialize;
 
 #[derive(Debug, Clone, Default, Deserialize)]
 pub struct Person {
