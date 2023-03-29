bcdd6d725e3fa6cbe0b0178e015d5bad60c99612
Marcos De la Cruz <50750885+marcosdepo@users.noreply.github.com>
2023-03-29 09:41:29 -0500

    WiP: reqwest POC

--- users_yew/src/users/users_state.rs
+++ users_yew/src/users/users_state.rs
@@ -1,3 +1,8 @@
+use std::fmt::format;
+
+use js_sys::JsString;
+use reqwest::header::{ACCESS_CONTROL_ALLOW_HEADERS, CONTENT_TYPE};
+use web_sys::console;
 use yew::Reducible;
 
 #[derive(PartialEq, Clone)]
@@ -22,6 +27,28 @@ impl Reducible for UserListState {
   fn reduce(self: std::rc::Rc<Self>, action: Self::Action) -> std::rc::Rc<Self> {
     match action {
       UserListAction::Add(name) => {
+        {
+          let name = name.clone();
+          wasm_bindgen_futures::spawn_local(async move {
+            let client = reqwest::Client::new();
+            let reqwest = client
+              .post(format!("{}/users", "http://localhost:3000"))
+              .header("Accept", "application/json")
+              .header(ACCESS_CONTROL_ALLOW_HEADERS, "*")
+              .header(CONTENT_TYPE, "application/json")
+              //.fetch_mode_no_cors()
+              .body(format!(r#"{{"username":"{name}"}}"#));
+            let resp = reqwest
+              .send()
+              .await
+              .expect("HTTP ERROR")
+              //.json::<Vec<QueryResult<T>>>()
+              .text()
+              .await
+              .expect("QueryResult parse error");
+            console::log_1(&JsString::from(resp.as_str()));
+          });
+        }
         let u = User {
           id: self.list.len().to_string(),
           name,
