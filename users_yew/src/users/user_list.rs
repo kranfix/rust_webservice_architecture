c668b33bf258625e4e50dec420259ba65b3d7280
jhonatan paucara <jhonatan.paucara@gmail.com>
2023-04-09 09:53:26 -0500

    WiP: Dialog opens with user information

--- users_yew/src/users/user_list.rs
+++ users_yew/src/users/user_list.rs
@@ -1,16 +1,30 @@
+use crate::users::{User, UserController};
+use web_sys::*;
 use yew::prelude::*;
-
-use crate::users::UserController;
 
 #[function_component]
 pub fn UserList() -> Html {
-  //let users = props.users.clone();
   let users = use_context::<UserController>().unwrap();
+  let user_to_edit: UseStateHandle<Option<User>> = use_state(|| None);
 
-  //let on_delete = props.on_delete.clone();
   let on_delete = {
     let users = users.clone();
     move |id: String| users.rm(id)
+  };
+  let dialog_ref = use_node_ref();
+  let open_update_user_dialog = {
+    let dialog_ref = dialog_ref.clone();
+    let user_to_edit = user_to_edit.clone();
+    move |user: User| {
+      let modal = dialog_ref.cast::<HtmlDialogElement>();
+
+      if let Some(modal) = modal {
+        match modal.show_modal() {
+          Ok(_) => user_to_edit.set(Some(user)),
+          Err(_) => user_to_edit.set(None),
+        }
+      }
+    }
   };
 
   {
@@ -30,10 +44,18 @@ pub fn UserList() -> Html {
             {
               users.list.iter().map(|u| {
                 let on_delete = on_delete.clone();
+                let open_update_user_dialog = open_update_user_dialog.clone();
                 let id = u.id.clone();
-                let onclick = {
+                let onclickx = {
+                  let id = id.clone();
                   move |_| {
                     on_delete.clone()(id.clone());
+                  }
+                };
+                let open_update_dialog = {
+                  let user = u.clone();
+                  move |_| {
+                    open_update_user_dialog.clone()(user.clone());
                   }
                 };
                 html!(
@@ -45,7 +67,8 @@ pub fn UserList() -> Html {
                         </li>
                       </div>
                       <div class="col mb-1">
-                        <button {onclick} type="button" class="btn btn-outline-danger">{ "X" }</button>
+                        <button onclick={open_update_dialog} type="button" class="btn btn-outline-danger">{ "U" }</button>
+                        <button onclick={onclickx} type="button" class="btn btn-outline-danger" style="margin-left:0.5rem">{ "X" }</button>
                       </div>
                     </div>
                   </div>
@@ -54,11 +77,27 @@ pub fn UserList() -> Html {
             }
           </ul>
         }
+        <dialog ref={dialog_ref}>
+          if let Some(user) = user_to_edit.as_ref().cloned() {
+            <UserEditCard {user}/>
+          }
+        </dialog>
       </div>
   }
 }
 
+#[derive(Properties, PartialEq)]
+pub struct Props {
+  pub user: User,
+}
+
 #[function_component]
-fn UserCard() -> Html {
-  html! {}
+fn UserEditCard(props: &Props) -> Html {
+  let user = props.user.clone();
+  html! {
+    <div>
+      <h2>{"Hola soy un dialog"}</h2>
+      <span>{user.name}</span>
+    </div>
+  }
 }
