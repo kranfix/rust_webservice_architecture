9b62f16f1913466f2e1269cbb98a667c38ac7d4c
Marcos De la Cruz <50750885+marcosdepo@users.noreply.github.com>
2023-04-29 12:03:54 -0500

    refactor unmutable user repo

--- axum_service/src/user_routes.rs
+++ axum_service/src/user_routes.rs
@@ -4,7 +4,6 @@ use axum::response::IntoResponse;
 use domain::UserRepo;
 use service_client::{CreateUserPayload, Reply, UpdateUserPayload, UserReply};
 use std::sync::Arc;
-use tokio::sync::Mutex;
 
 #[macro_export]
 macro_rules! create_user_routes {
@@ -25,13 +24,10 @@ macro_rules! create_user_routes {
 }
 
 pub async fn create_user<UR: UserRepo>(
-  State(state): State<Arc<Mutex<UR>>>,
+  State(user_repo): State<Arc<UR>>,
   Json(payload): Json<CreateUserPayload>,
 ) -> impl IntoResponse {
-  let created_user = {
-    let mut user_repo = state.lock().await;
-    user_repo.create_user(payload.username).await
-  };
+  let created_user = user_repo.create_user(payload.username).await;
   match created_user {
     Ok(u) => {
       let user_reply: UserReply = u.into();
@@ -48,12 +44,9 @@ pub async fn create_user<UR: UserRepo>(
 }
 
 pub async fn get_users<UR: UserRepo>(
-  State(state): State<Arc<Mutex<UR>>>,
+  State(user_repo): State<Arc<UR>>,
 ) -> (StatusCode, Json<Reply<Vec<UserReply>>>) {
-  let users_result = {
-    let state = state.lock().await;
-    state.get_users().await
-  };
+  let users_result = user_repo.get_users().await;
 
   let users = match users_result {
     Ok(users) => users,
@@ -69,13 +62,10 @@ pub async fn get_users<UR: UserRepo>(
 }
 
 pub async fn get_user_by_id<UR: UserRepo>(
-  State(user_repo): State<Arc<Mutex<UR>>>,
+  State(user_repo): State<Arc<UR>>,
   Path(id): Path<String>,
 ) -> impl IntoResponse {
-  let result = {
-    let user_repo = user_repo.lock().await;
-    user_repo.get_user_by_id(id).await
-  };
+  let result = user_repo.get_user_by_id(id).await;
   let err = match result {
     Ok(user) => return (StatusCode::OK, Json(Reply::Data(UserReply::from(user)))),
     Err(e) => e,
@@ -89,13 +79,10 @@ pub async fn get_user_by_id<UR: UserRepo>(
 }
 
 pub async fn delete_user<UR: UserRepo>(
-  State(user_repo): State<Arc<Mutex<UR>>>,
+  State(user_repo): State<Arc<UR>>,
   Path(id): Path<String>,
 ) -> impl IntoResponse {
-  let res = {
-    let mut user_repo = user_repo.lock().await;
-    user_repo.delete_user(id).await
-  };
+  let res = user_repo.delete_user(id).await;
   let err = match res {
     Ok(user) => return (StatusCode::OK, Json(Reply::Data(UserReply::from(user)))),
     Err(e) => e,
@@ -109,14 +96,11 @@ pub async fn delete_user<UR: UserRepo>(
 }
 
 pub async fn update_user<UR: UserRepo>(
-  State(user_repo): State<Arc<Mutex<UR>>>,
+  State(user_repo): State<Arc<UR>>,
   Path(id): Path<String>,
   Json(payload): Json<UpdateUserPayload>,
 ) -> impl IntoResponse {
-  let res = {
-    let mut user_repo = user_repo.lock().await;
-    user_repo.update_user(id, payload.username).await
-  };
+  let res = user_repo.update_user(id, payload.username).await;
   let err = match res {
     Ok(user) => return (StatusCode::OK, Json(Reply::Data(UserReply::from(user)))),
     Err(e) => e,
