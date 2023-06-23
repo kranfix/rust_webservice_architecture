cb420bf10590f15148c194c4cc09d6d564f0ca1b
Marcos De la Cruz <50750885+marcosdepo@users.noreply.github.com>
2023-06-22 23:27:30 -0500

    finished cuyk edit user
    Co-authored-by: Frank Moreno <frankmoreno1993@gmail.com>
    Co-authored-by: Jhonatan Paucara <JhonatanPaucara@users.noreply.github.com>

--- qwik-app/src/routes/users/index.tsx
+++ qwik-app/src/routes/users/index.tsx
@@ -9,7 +9,13 @@ import {
 } from "@builder.io/qwik";
 //import styles from "../users/index.module.css";
 import { Form, routeAction$, routeLoader$, zod$ } from "@builder.io/qwik-city";
-import { User, createUser, deleteUserById, getUsers } from "../../users-client";
+import {
+  User,
+  createUser,
+  deleteUserById,
+  getUsers,
+  updateUserById,
+} from "../../users-client";
 import { z } from "zod";
 import { style, styled } from "styled-vanilla-extract/qwik";
 import { Row } from "./styles.css";
@@ -39,34 +45,92 @@ export const useDeleteUserById = routeAction$(
   })
 );
 
+export const useUpdateUserById = routeAction$(
+  async (props) => {
+    console.log("useUpdateUserById", props.id);
+    return await updateUserById(props.id, props.newUsername);
+  },
+  zod$({
+    id: z.string().trim(),
+    newUsername: z.string().trim(),
+  })
+);
+
 interface UserListProps {
   users: User[];
 }
 
 export const UserList = component$<UserListProps>(({ users }) => {
   const deleteUserById = useDeleteUserById();
+  const selectedUser = useSignal<User | null>(null);
+  const modalStore = useStore({
+    isOpen: false,
+  });
   return (
-    <ul>
-      {users.map((user) => (
-        <li>
-          <Form
-            action={deleteUserById}
-            onSubmitCompleted$={() => {
-              console.log("submit completed", user.id);
-            }}
-          >
+    <>
+      <ul>
+        {users.map((user) => (
+          <li>
             <Row>
               <div>
                 <p>{user.id}</p>
                 <p>{user.username}</p>
               </div>
-              <input type="hidden" name="id" value={user.id} />
-              <CuykButton>X</CuykButton>
+
+              <CuykButton
+                onClick$={() => {
+                  selectedUser.value = user;
+                  modalStore.isOpen = true;
+                }}
+              >
+                PC
+              </CuykButton>
+
+              <Form
+                action={deleteUserById}
+                onSubmitCompleted$={() => {
+                  console.log("submit completed", user.id);
+                }}
+              >
+                <input type="hidden" name="id" value={user.id} />
+                <CuykButton>X</CuykButton>
+              </Form>
             </Row>
-          </Form>
-        </li>
-      ))}
-    </ul>
+          </li>
+        ))}
+      </ul>
+      <Modal title={"Edit User"} store={modalStore}>
+        {selectedUser.value && (
+          <EditUser
+            user={selectedUser.value}
+            onUpdate={$(() => {
+              modalStore.isOpen = false;
+            })}
+          />
+        )}
+      </Modal>
+    </>
+  );
+});
+
+interface EditUserProps {
+  user: User;
+  onUpdate: QRL<() => void>;
+}
+
+const EditUser = component$<EditUserProps>(({ user, onUpdate }) => {
+  const username = useSignal(user.username);
+  const updateUserById = useUpdateUserById();
+  const disableUpdate = useComputed$(() => {
+    const trimmed = username.value.trim();
+    return trimmed.length == 0 || trimmed == user.username;
+  });
+  return (
+    <Form action={updateUserById} onSubmit$={onUpdate}>
+      <input type="hidden" name="id" value={user.id} />
+      <input type="text" name="newUsername" bind:value={username} />
+      <CuykButton disabled={disableUpdate.value}>Update</CuykButton>
+    </Form>
   );
 });
 
@@ -195,9 +259,61 @@ interface CuykButtonProps {
 export const CuykButton = component$<CuykButtonProps>(
   ({ disabled, onClick$ }) => {
     return (
-      <button disabled={disabled ?? false} onClick$={onClick$}>
+      <button disabled={disabled} onClick$={onClick$}>
         <Slot />
       </button>
     );
   }
 );
+
+///////////
+
+export interface ModalStore {
+  isOpen: boolean;
+}
+
+export interface ModalProps {
+  title: string;
+  store: ModalStore;
+}
+
+export const Modal = component$(({ title, store }: ModalProps) => {
+  if (store.isOpen)
+    return (
+      <div
+        class="relative z-overlay"
+        aria-labelledby={title}
+        role="dialog"
+        aria-modal="true"
+      >
+        <Overlay />
+
+        <div class="fixed inset-0 overflow-y-auto">
+          <div class="flex items-center justify-center min-h-full p-4 text-center sm:items-center sm:p-0">
+            <Panel>
+              <button onClick$={() => (store.isOpen = false)}>
+                close modal
+              </button>
+              <Slot />
+            </Panel>
+          </div>
+        </div>
+      </div>
+    );
+
+  return null;
+});
+
+export const Overlay = component$(() => {
+  return (
+    <div class="fixed inset-0 transition-opacity bg-black bg-opacity-40 backdrop-blur-sm" />
+  );
+});
+
+export const Panel = component$(() => {
+  return (
+    <div class="relative p-8 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:w-full sm:max-w-lg">
+      <Slot />
+    </div>
+  );
+});
--- qwik-app/src/users-client/index.tsx
+++ qwik-app/src/users-client/index.tsx
@@ -70,3 +70,29 @@ export const deleteUserById = async (id: string): Promise<User | Error> => {
   };
   return result.data;
 };
+
+export const updateUserById = async (
+  id: string,
+  username: string
+): Promise<User | Error> => {
+  let response: Response;
+  try {
+    response = await fetch(`http://127.0.0.1:3000/users/` + id, {
+      method: "PUT",
+      body: JSON.stringify({ username }),
+      headers: { "content-type": "application/json" },
+    });
+  } catch {
+    return new Error("Server did not response");
+  }
+
+  if (!response.ok) {
+    console.log(`[updateUserById] ${response.status} ${response.body}`);
+    return new Error("");
+  }
+
+  const result = (await response.json()) as {
+    data: User;
+  };
+  return result.data;
+};
